46fb67e57880a782c7d6306dcbdfeb5e568f1227
CassieV <obicassievirtue@gmail.com>
2022-07-13 17:33:14 +0200

    Add space to gsub to seperate characters - "how's"

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,7 @@
 def substrings(word, list_of_words)
   # split words and remove special characters with "gsub(/[^a-zA-Z0-9\ ]/,"")"
   # the space at the end of "9\" is to keep the spaces between words
-  words_array = word.downcase.gsub(/[^a-zA-Z0-9\ ]/,"").split
+  words_array = word.downcase.gsub(/[^a-zA-Z0-9\ ]/," ").split
   words_array.keep_if {|v| list_of_words.include?(v)}
   words = words_array.reduce(Hash.new(0)) do |result, count|
     result[count] += 1
@@ -14,4 +14,4 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 substrings("Howdy partner, where is it going, sit down low! How's it going?", dictionary)
 
-# => {"howdy"=>1, "partner"=>1, "it"=>2, "going"=>2, "sit"=>1, "down"=>1, "low"=>1}
+# => {"howdy"=>1, "partner"=>1, "it"=>2, "going"=>2, "sit"=>1, "down"=>1, "low"=>1, "how"=>1}
