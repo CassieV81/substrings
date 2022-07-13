0e00cd154b48afb983acf3269477c4bea1b667f4
CassieV <obicassievirtue@gmail.com>
2022-07-13 13:28:50 +0200

    Initial commit

---
+++ substrings.rb
@@ -0,0 +1,7 @@
+def substrings(word, list_of_words)
+  p list_of_words.include?(word)
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("up", dictionary)
