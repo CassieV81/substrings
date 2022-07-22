be209d6d99fe42e8cd4da49b4191b3515e96ba97
CassieV <obicassievirtue@gmail.com>
2022-07-22 15:28:33 +0200

    Replace reduce with each_with_object Rubocop suggested each_with_object and autocorrected minor issues

--- substrings.rb
+++ substrings.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 def substrings(word, list_of_words)
   # split words and remove special characters with "gsub(/[^a-zA-Z0-9\ ]/,"")"
   # the space at the end of "9\" is to keep the spaces between words
-  words_array = word.downcase.gsub(/[^a-zA-Z0-9\ ]/," ").split
-  words_array.keep_if {|v| list_of_words.include?(v)}
-  words = words_array.reduce(Hash.new(0)) do |result, count|
+  words_array = word.downcase.gsub(/[^a-zA-Z0-9\ ]/, ' ').split
+  words_array.keep_if { |v| list_of_words.include?(v) }
+  words = words_array.each_with_object(Hash.new(0)) do |count, result|
     result[count] += 1
-    result
   end
   puts words
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 substrings("Howdy partner, where is it going, sit down low! How's it going?", dictionary)
 
